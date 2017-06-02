9d35872d1e62169816e10bd9fc4b271c4de975fe
ZB0106 <miaocaiwang@miaocaiwangdeiMac.local>
2017-06-02 18:10:22 +0800

    111111

---
+++ ZBActionDemo/ZBActionDemo/KDActionSheet/FontMacro.h
@@ -0,0 +1,36 @@
+//
+//  FontMacro.h
+//  CBExtension
+//
+//  Created by  ly on 13-6-14.
+//  Copyright (c) 2013年 Lei Yan. All rights reserved.
+//
+
+#ifndef FontMacro_h
+#define FontMacro_h
+
+#define SystemFontOfSize(size)      [UIFont systemFontOfSize:(size)]
+#define BoldSystemFontOfSize(size)  [UIFont boldSystemFontOfSize:(size)]
+
+
+
+//字体
+#define Font8           SystemFontOfSize(8.0f)
+#define Font9           SystemFontOfSize(9.0f)
+#define Font10          SystemFontOfSize(10.0f)
+#define Font11          SystemFontOfSize(11.0f)
+#define Font12          SystemFontOfSize(12.0f)
+#define Font13          SystemFontOfSize(13.0f)
+#define Font14          SystemFontOfSize(14.0f)
+#define Font15          SystemFontOfSize(15.0f)
+#define Font16          SystemFontOfSize(16.0f)
+#define Font17          SystemFontOfSize(17.0f)
+#define Font18          SystemFontOfSize(18.0f)
+
+
+#define SpecialFont             Font16    //界面所用大字号
+#define ContentLightFont        Font12    //界面所用小字号
+#define ContentDarkFont         Font14    //界面所用中字号
+
+
+#endif
--- ZBActionDemo/ZBActionDemo/KDActionSheet/KDActionButton.m
+++ ZBActionDemo/ZBActionDemo/KDActionSheet/KDActionButton.m
@@ -7,22 +7,80 @@
 //
 
 #import "KDActionButton.h"
+#import "ColorMacro.h"
+#import "FontMacro.h"
 
 @implementation KDActionButton
-
-//- (void)drawRect:(CGRect)rect
-//{
-//    CGContextRef context = UIGraphicsGetCurrentContext();
-//    CGContextSetRGBStrokeColor(context, 128 / 255.0, 128 / 255.0, 128 / 255.0, 1);
-//    CGContextSetLineWidth(context, 0.5);
-//    CGContextMoveToPoint(context, rect.origin.x, rect.origin.y + rect.size.height);
-//    CGContextAddLineToPoint(context, rect.origin.x + rect.size.width, rect.origin.y + rect.size.height);
-//    CGContextStrokePath(context);
-//}
 
 - (void)setHighlighted:(BOOL)highlighted
 {
     
+}
+
++ (instancetype)kdActionButtonWithButtonType:(ZB_buttonType)buttonType action:(buttonAction)action
+{
+    ZB_ButtonItem *item = [ZB_ButtonItem zb_buttonItemWithButtonType:buttonType];
+    KDActionButton *btn = [KDActionButton buttonWithType:UIButtonTypeCustom];
+    [btn setTitleColor:item.titleColor forState:UIControlStateNormal];
+    btn.titleLabel.font = item.titleFont;
+    btn.adjustsImageWhenHighlighted = NO;
+    [btn setExclusiveTouch:YES];/*禁用按钮同时点击*/
+    btn.backgroundColor = ClearColor;
+    btn.action = action;
+
+
+    return btn;
+}
+- (instancetype)init
+{
+    if (self = [super init]) {
+        ZB_ButtonItem *item = [ZB_ButtonItem zb_buttonItemWithButtonType:ZB_buttonDefault];
+        [self setTitleColor:item.titleColor forState:UIControlStateNormal];
+        self.titleLabel.font = item.titleFont;
+         self.backgroundColor = ClearColor;
+        self.adjustsImageWhenHighlighted = NO;
+        [self setExclusiveTouch:YES];/*禁用按钮同时点击*/
+
+    }
+    return self;
+}
+
+@end
+
+@implementation ZB_ButtonItem
+
++ (instancetype)zb_buttonItemWithButtonType:(ZB_buttonType)buttonType
+{
+    ZB_ButtonItem *item = [[ZB_ButtonItem alloc] init];
+    switch (buttonType) {
+        case ZB_buttonOK:
+        {
+            item.titleColor = BlueColor;
+            item.titleFont = Font14;
+        }
+            break;
+        case ZB_buttonCancel:
+        {
+            item.titleColor = BlackColor;
+            item.titleFont = Font14;
+        }
+            break;
+        case ZB_buttonDestroy:
+        {
+            item.titleColor = RedColor;
+            item.titleFont = Font14;
+        }
+            break;
+
+        case ZB_buttonDefault:
+        {
+            item.titleColor = RGBCOLORWITH16COLOR(0x2f93e7);
+            item.titleFont = Font14;
+        }
+
+            break;
+    }
+    return item;
 }
 
 @end
